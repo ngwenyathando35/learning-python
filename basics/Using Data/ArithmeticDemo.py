dd802d467f8ebb2153c1c7d1bb7f19d24757b5cb
thandongwenya35 <47686283+ngwenyathando35@users.noreply.github.com>
2026-01-29 09:57:58 +0200

    Create ArithmeticDemo.py
    Testing the uses of arithmetic symbols

---
+++ basics/Using Data/ArithmeticDemo.py
@@ -0,0 +1,10 @@
+firstNumber = int(input("Please enter an integer >> "))
+secondNumber = int(input("Please enter another integer >> "))
+
+sum = firstNumber + secondNumber
+difference = firstNumber - secondNumber
+average = sum/2
+
+print(f"{firstNumber} + {secondNumber} is {sum}")
+print(f"{firstNumber} - {secondNumber} is {difference}")
+print(f"The average of {firstNumber} and {secondNumber} is {average}")
