73b65ca04de2bd146e66402694cca9adbd312328
thandongwenya35 <47686283+ngwenyathando35@users.noreply.github.com>
2026-01-29 09:09:26 +0200

    Create IntegerDemoInteractive.py
    Learned about Python numerical data types, how to accept user input and how to display it

---
+++ basics/Using Data/IntegerDemoInteractive.py
@@ -0,0 +1,15 @@
+# Python only has 4 data types namely, interger, float, complex, bool
+
+anInt = int(input("Please enter an integer >> "))
+aFloat = float(input("Please enter a float >> "))
+aComplex = complex(input("Please enter a complex (e.g 3 + 4j)>> "))
+aBool = bool(input("Please enter a boolean value >> "))
+
+print("The int is " + str(anInt))
+print("The float is " + str(aFloat))
+print(f"The complex is {aComplex}")
+print("The boolean value is " + str(aBool))
+
+
+# A more cleaner way to print numbers is 
+# print(f"Your age is {age})
